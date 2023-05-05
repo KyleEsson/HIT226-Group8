883722724b270b0eceb994011e5241704848d3ad
austin1912 <austinwiwarsono@gmail.com>
2023-05-05 12:30:24 +0930

    editing all bookings(For CSS is on line 400 to the end)

--- scripts/bookings.js
+++ scripts/bookings.js
@@ -7,3 +7,13 @@ function myFunction() {
         x.style.display = "block";
     }
 }
+
+const wrapper = document.querySelector(".wrapper"),
+              signupHeader = document.querySelector(".signup header"),
+              loginHeader = document.querySelector(".login header");
+            loginHeader.addEventListener("click", () => {
+              wrapper.classList.add("active");
+            });
+            signupHeader.addEventListener("click", () => {
+              wrapper.classList.remove("active");
+            });
