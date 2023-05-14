138c415ed2f7e64e2192fd6c09980e3fef5fc3ff
KyleEsson <89835575+KyleEsson@users.noreply.github.com>
2023-05-15 09:25:48 +0930

    skeleton styling
    also some js work on stories

--- scripts/stories.js
+++ scripts/stories.js
@@ -73,6 +73,17 @@ document.addEventListener('DOMContentLoaded', function () {
                 const modalElement = document.createElement('dialog');
                 modalElement.classList.add('storyModal');
                 modalElement.innerHTML = '<button class="shareClose" id="storyClose" onclick="closeStory()" ><i class="fa fa-close"></i></button>';
+                modalElement.setAttribute("data-backdrop", "static");
+                modalElement.setAttribute("data-keyboard", "false");
+                // modalElement.innerHTML =
+                //     `<div class="wave">
+                //     <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                //     <path
+                //         d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+                //         class="shape-fill"
+                //     ></path>
+                //     </svg>
+                // </div>`;
                 const img = '<img src="images/' + storyData[i].avatar + '.png" alt="Avatar">';
                 modalElement.insertAdjacentHTML('beforeend', img);
                 const namePost = `<h3>` + storyData[i].name + `<h3>`;
@@ -83,12 +94,11 @@ document.addEventListener('DOMContentLoaded', function () {
                 object.appendChild(modalElement);
                 const storyModal = document.querySelector('.storyModal');
                 storyModal.showModal();
+
             }
             divElement.innerHTML = '<img src="images/' + storyData[i].avatar + '.png" alt="Avatar">';
             const namePost = `<h3>` + storyData[i].name + `<h3>`;
             divElement.insertAdjacentHTML('beforeend', namePost);
-            // const breakLine = `<div class="cardLine"></div>`;
-            // divElement.insertAdjacentHTML('beforeend', breakLine);
             const storyPost = `<p>` + storyData[i].intro + `</p>`;
             divElement.insertAdjacentHTML('beforeend', storyPost);
             const readStory = '<button class="readStory">Read Story</button>';
@@ -104,13 +114,4 @@ function closeStory() {
 }
 
 
-/* Integrating Google Translate into the navigation bar */
 
-// NOT CURRENTLY WORKING
-
-// function googleTranslateElementInit() {
-//     new google.translate.TranslateElement(
-//         { pageLanguage: 'en' },
-//         'google_translate_element'
-//     );
-// }
