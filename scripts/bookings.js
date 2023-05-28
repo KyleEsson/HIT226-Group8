893db29152994bba8a8a37ff34e2c7bd6899102d
austin1912 <austinwiwarsono@gmail.com>
2023-05-28 12:00:48 +0930

    js stuffs

--- scripts/bookings.js
+++ scripts/bookings.js
@@ -8,16 +8,33 @@ function navFunction() {
   }
 }
 
-const wrapper = document.querySelector(".wrapper"),
-signupHeader = document.querySelector(".signup header");
+function myfunc(event){
+  event.preventDefault();
+
+  var uid = document.getElementById("username").value;
+  var pw = document.getElementById("password").value;
+  var pw = document.getElementById("confirm_password").value;
+  var email = document.getElementById("email").value;
+  var date = document.getElementById("d1").value;
   
+  var freeform = document.getElementById("freeform").value;
+  var address = document.getElementById("address").value;
 
-signupHeader.addEventListener("click", () => {
-  wrapper.classList.remove("active");
-});
+  localStorage.setItem('ls_username', username);
+  localStorage.setItem('ls_password', password);
+  localStorage.setItem('ls_confirm_password', confirm_password);
+  localStorage.setItem('ls_email', email);
+  localStorage.setItem('ls_d1', d1);
+  
+  localStorage.setItem('ls_freeform', freeform);
+  localStorage.setItem('ls_address', address);
+
+
+}
 
 
 
+                  
 
 /* Integrating Google Translate into the navigation bar */
 
